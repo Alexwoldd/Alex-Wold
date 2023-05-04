56e43829367ed392a4d3fd0f506e5e0e2a46fea0
Alexwoldd <Woldal@oregonstate.edu>
2023-05-04 22:54:59 +0000

    Lab 4, questions 1-6 not 5

--- MatplotlibIntro/matplotlib_intro.py
+++ MatplotlibIntro/matplotlib_intro.py
@@ -1,91 +1,83 @@
 # matplotlib_intro.py
 """Python Essentials: Intro to Matplotlib.
-<Name>
-<Class>
-<Date>
+<Alexandra Wold>
+<MTH 420>
+<5/5/23>
 """
-
+import numpy as np
+from matplotlib import pyplot as plt
 
 # Problem 1
 def var_of_means(n):
-    """ Create an (n x n) array of values randomly sampled from the standard
-    normal distribution. Compute the mean of each row of the array. Return the
-    variance of these means.
-
-    Parameters:
-        n (int): The number of rows and columns in the matrix.
-
-    Returns:
-        (float) The variance of the means of each row.
-    """
-    raise NotImplementedError("Problem 1 Incomplete")
+    A = np.random.normal(size=(n,n))
+    A_mean=A.mean(axis=1)
+    return A_mean.var
 
 def prob1():
-    """ Create an array of the results of var_of_means() with inputs
-    n = 100, 200, ..., 1000. Plot and show the resulting array.
-    """
-    raise NotImplementedError("Problem 1 Incomplete")
+    var_array=np.zeros((1,10))
+    for i in range(10):
+        var_array=var_of_means(100*(i+1))
+    plt.plot(var_array)
+    return plt.show()
+    
 
 
 # Problem 2
 def prob2():
-    """ Plot the functions sin(x), cos(x), and arctan(x) on the domain
-    [-2pi, 2pi]. Make sure the domain is refined enough to produce a figure
-    with good resolution.
-    """
-    raise NotImplementedError("Problem 2 Incomplete")
+    a=np.linspace(-2*np.pi,2*np.pi,200)
+    plt.plot(a,np.sin(a))
+    plt.plot(a,np.cos(a))
+    plt.plot(a,np.arctan(a))
+    return plt.show()
 
 
 # Problem 3
 def prob3():
-    """ Plot the curve f(x) = 1/(x-1) on the domain [-2,6].
-        1. Split the domain so that the curve looks discontinuous.
-        2. Plot both curves with a thick, dashed magenta line.
-        3. Set the range of the x-axis to [-2,6] and the range of the
-           y-axis to [-6,6].
-    """
-    raise NotImplementedError("Problem 3 Incomplete")
+    a1=np.linspace(-2,1,45, endpoint=False)
+    a2=np.linspace(1,6,75)[1:]
+    plt.plot(a1, (1/(a1-1)), 'm--',linewidth=4)
+    plt.plot(a2, (1/(a2-1)), 'm--',linewidth=4)
+    plt.xlim(-2,6)
+    plt.ylim(-6,6)
+    return plt.show
 
 
 # Problem 4
 def prob4():
-    """ Plot the functions sin(x), sin(2x), 2sin(x), and 2sin(2x) on the
-    domain [0, 2pi], each in a separate subplot of a single figure.
-        1. Arrange the plots in a 2 x 2 grid of subplots.
-        2. Set the limits of each subplot to [0, 2pi]x[-2, 2].
-        3. Give each subplot an appropriate title.
-        4. Give the overall figure a title.
-        5. Use the following line colors and styles.
-              sin(x): green solid line.
-             sin(2x): red dashed line.
-             2sin(x): blue dashed line.
-            2sin(2x): magenta dotted line.
-    """
-    raise NotImplementedError("Problem 4 Incomplete")
+    x=np.linspace(0,2*np.pi,200)
+    plt.subplot(2,2,1)
+    plt.plot(x,np.sin(x), 'g-')
+    plt.title("a")
+    plt.subplot(2,2,2)
+    plt.plot(x,np.sin(2*x), 'r--')
+    plt.plot("b")
+    plt.subplot(2,2,3)
+    plt.plot(x,2*np.sin(x), 'b-')
+    plt.plot("c")
+    plt.subplot(2,2,4)
+    plt.plot(x, 2*np.sin(2*x), 'm:')
+    plt.title("d")
+    
+    plt.axis([0,2*np.pi,-2,2])
+    plt.suptitle("abcd")
+    return plt.show()
 
 
 # Problem 5
 def prob5():
-    """ Visualize the data in FARS.npy. Use np.load() to load the data, then
-    create a single figure with two subplots:
-        1. A scatter plot of longitudes against latitudes. Because of the
-            large number of data points, use black pixel markers (use "k,"
-            as the third argument to plt.plot()). Label both axes.
-        2. A histogram of the hours of the day, with one bin per hour.
-            Label and set the limits of the x-axis.
-    """
-    raise NotImplementedError("Problem 5 Incomplete")
+
 
 
 # Problem 6
 def prob6():
-    """ Plot the function g(x,y) = sin(x)sin(y)/xy on the domain
-    [-2pi, 2pi]x[-2pi, 2pi].
-        1. Create 2 subplots: one with a heat map of g, and one with a contour
-            map of g. Choose an appropriate number of level curves, or specify
-            the curves yourself.
-        2. Set the limits of each subplot to [-2pi, 2pi]x[-2pi, 2pi].
-        3. Choose a non-default color scheme.
-        4. Include a color scale bar for each subplot.
-    """
-    raise NotImplementedError("Problem 6 Incomplete")
+    a=np.linspace(-2*np.pi,2*np.pi, 200)
+    b=a.copy()
+    c=(((np.sin(a))*(np.sin(b)))/(a*b))
+    plt.subplot(1,2,1)
+    plt.pcolormesh(a,b,c, cmap="magma", shading="auto")
+    plt.colorbar()
+    plt.subplot(1,2,2)
+    plt.contour(a,b,c,20, cmap="magma")
+    plt.colorbar()
+    plt.axis([-2*np.pi,2*np.pi,-2*np.pi,2*np.pi])
+    return plt.show()
